df407d8ef15f53a2c9c2946bf84c4025b1f4e7d1
Diego Henrique <diegohrp20@gmail.com>
2024-06-12 15:10:46 -0300

    fix: Adicionando classe service para criações de regras de negócios para o cliente, além de criar metodo patch para modificar parcialmente um cliente e put para alterar todos os campos do cliente. Além de retirar a persistência em memória da classe CadastroClienteResource, tendo a regra no service.

--- src/main/java/org/quarkusclub/CadastroClienteResource.java
+++ src/main/java/org/quarkusclub/CadastroClienteResource.java
@@ -2,27 +2,30 @@ package org.quarkusclub;
 
 import org.jboss.resteasy.reactive.RestResponse;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import java.util.List;
-import java.util.ArrayList;
 import java.util.UUID;
 
 @Path("/v1/clientes")
 public class CadastroClienteResource {
 
-    // Nossa persistência em memória
-    List<ClienteDTO> clientes = new ArrayList<>();
+    @Inject
+    CadastroClienteService cadastroClienteService;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public RestResponse<List<ClienteDTO>> consultarClientes() {
+        List<ClienteDTO> clientes = cadastroClienteService.consultarClientes();
         return RestResponse.status(Response.Status.OK, clientes);
     }
 
@@ -36,16 +39,15 @@ public class CadastroClienteResource {
             return RestResponse.status(RestResponse.Status.BAD_REQUEST, responseCliente);
         }
 
-        cliente.setId(UUID.randomUUID());
-        clientes.add(cliente);
+        ClienteDTO novoCliente = cadastroClienteService.criarCliente(cliente);
 
-        return RestResponse.status(Response.Status.CREATED, cliente);
+        return RestResponse.status(Response.Status.CREATED, novoCliente);
     }
 
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public RestResponse<ClienteDTO> atualizarCliente(ClienteDTO cliente) {
+    public RestResponse<ClienteDTO> atualizarTodosCamposCliente(ClienteDTO cliente) {
         if(cliente == null) {
             ClienteDTO responseCliente = new ClienteDTO();
             responseCliente.setResponseMessage("Cliente nao informado");
@@ -58,18 +60,40 @@ public class CadastroClienteResource {
             return RestResponse.status(RestResponse.Status.BAD_REQUEST, responseCliente);
         }
 
-        for(ClienteDTO c : clientes) {
-            if(c.getId().equals(cliente.getId())) {
-                c.setNome(cliente.getNome());
-                c.setCpf(cliente.getCpf());
-                c.setEndereco(cliente.getEndereco());
-                return RestResponse.status(Response.Status.OK, c);
-            }
+        ClienteDTO clienteAtualizado = cadastroClienteService.atualizarTodosCamposCliente(cliente);
+        if(clienteAtualizado == null) {
+            ClienteDTO responseCliente = new ClienteDTO();
+            responseCliente.setResponseMessage("Cliente nao encontrado");
+            return RestResponse.status(RestResponse.Status.NOT_FOUND, responseCliente);
         }
 
-        ClienteDTO responseCliente = new ClienteDTO();
-        responseCliente.setResponseMessage("Cliente nao encontrado");
+        return RestResponse.status(Response.Status.OK, clienteAtualizado);
+    }
 
-        return RestResponse.status(Response.Status.NOT_FOUND, responseCliente);
+    @PATCH
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/{idCliente}")
+    public RestResponse<ClienteDTO> atualizarParcialCliente(@PathParam("idCliente") UUID idCliente, ClienteDTO cliente) {
+        if(cliente == null) {
+            ClienteDTO responseCliente = new ClienteDTO();
+            responseCliente.setResponseMessage("Cliente nao informado");
+            return RestResponse.status(RestResponse.Status.BAD_REQUEST, responseCliente);
+        }
+
+        if(idCliente == null) {
+            ClienteDTO responseCliente = new ClienteDTO();
+            responseCliente.setResponseMessage("ID do cliente nao informado");
+            return RestResponse.status(RestResponse.Status.BAD_REQUEST, responseCliente);
+        }
+
+        ClienteDTO clienteAtualizado = cadastroClienteService.atualizarParcialCliente(idCliente, cliente);
+        if(clienteAtualizado == null) {
+            ClienteDTO responseCliente = new ClienteDTO();
+            responseCliente.setResponseMessage("Cliente nao encontrado");
+            return RestResponse.status(RestResponse.Status.NOT_FOUND, responseCliente);
+        }
+
+        return RestResponse.status(Response.Status.OK, clienteAtualizado);
     }
 }
---
+++ src/main/java/org/quarkusclub/CadastroClienteService.java
@@ -0,0 +1,78 @@
+package org.quarkusclub;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@ApplicationScoped
+public class CadastroClienteService {
+
+    List<ClienteDTO> clientes = new ArrayList<>();
+
+    public ClienteDTO criarCliente(ClienteDTO cliente) {
+        cliente.setId(UUID.randomUUID());
+        clientes.add(cliente);
+
+        return cliente;
+    }
+
+    public ClienteDTO atualizarParcialCliente(UUID idCliente, ClienteDTO cliente) {
+        for(ClienteDTO c : clientes) {
+            if(c.getId().equals(idCliente)) {
+                if(cliente.getNome() != null) {
+                    c.setNome(cliente.getNome());
+                }
+                if(cliente.getCpf() != null) {
+                    c.setCpf(cliente.getCpf());
+                }
+                if(cliente.getEndereco() != null) {
+                    c.setEndereco(cliente.getEndereco());
+                }
+                if(cliente.getCidade() != null) {
+                    c.setCidade(cliente.getCidade());
+                }
+                if(cliente.getEstado() != null) {
+                    c.setEstado(cliente.getEstado());
+                }
+                if(cliente.getEmail() != null) {
+                    c.setEmail(cliente.getEmail());
+                }
+                if(cliente.getTelefone() != null) {
+                    c.setTelefone(cliente.getTelefone());
+                }
+                if(cliente.getNomePlano() != null) {
+                    c.setNomePlano(cliente.getNomePlano());
+                }
+                if(cliente.getIndicacao() != null) {
+                    c.setIndicacao(cliente.getIndicacao());
+                }
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public ClienteDTO atualizarTodosCamposCliente(ClienteDTO cliente) {
+        for(ClienteDTO c : clientes) {
+            if(c.getId().equals(cliente.getId())) {
+                c.setNome(cliente.getNome());
+                c.setCpf(cliente.getCpf());
+                c.setEndereco(cliente.getEndereco());
+                c.setCidade(cliente.getCidade());
+                c.setEstado(cliente.getEstado());
+                c.setEmail(cliente.getEmail());
+                c.setTelefone(cliente.getTelefone());
+                c.setNomePlano(cliente.getNomePlano());
+                c.setIndicacao(cliente.getIndicacao());
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public List<ClienteDTO> consultarClientes() {
+        return clientes;
+    }
+
+}
